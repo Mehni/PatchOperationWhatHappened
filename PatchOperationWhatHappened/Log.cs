c9b6197fd7daf157af667e15f5373f778dd3acfc
Mehni <Mehni@users.noreply.github.com>
2019-05-28 22:45:48 +0200

    Add project files.

---
+++ PatchOperationWhatHappened/Log.cs
@@ -0,0 +1,27 @@
+﻿
+namespace PatchOperationWhatHappened
+{
+    using System;
+    using System.Xml;
+    using Verse;
+
+    public class Log : PatchOperationPathed
+    {
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            try
+            {
+                foreach (var current in xml.SelectNodes(xpath))
+                {
+                    XmlNode xmlNode = current as XmlNode;
+                    Verse.Log.Message(xmlNode?.OuterXml);
+                }
+            }
+            catch (Exception ex)
+            {
+                Verse.Log.Message($" FAIL: {xpath} + {ex}");
+            }
+            return true;
+        }
+    }
+}
